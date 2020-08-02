fe785c0a340f7ea880797f451c778eb748bc1afa
Nicolás Medero <nicolasmederoc@gmail.com>
2020-08-02 05:32:56 -0300

    improve some details

--- app/src/main/java/utn/frba/mobile/codechallenge/views/search/SearchFragment.kt
+++ app/src/main/java/utn/frba/mobile/codechallenge/views/search/SearchFragment.kt
@@ -1,7 +1,6 @@
 package utn.frba.mobile.codechallenge.views.search
 
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
