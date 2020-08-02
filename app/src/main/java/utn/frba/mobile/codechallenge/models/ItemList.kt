1bfe9b18ac8a94134b16cff019a2c0b9a2e6d565
Nicolás Medero <nicolasmederoc@gmail.com>
2020-08-02 05:21:43 -0300

    solve error when user clicks on 'tasks' device button

--- app/src/main/java/utn/frba/mobile/codechallenge/models/ItemList.kt
+++ app/src/main/java/utn/frba/mobile/codechallenge/models/ItemList.kt
@@ -1,8 +1,10 @@
 package utn.frba.mobile.codechallenge.models
 
+import java.io.Serializable
+
 data class ItemList (
     val title: String,
     val price: Float,
     val thumbnail: String,
     var like: Boolean = false
-)
+) : Serializable
